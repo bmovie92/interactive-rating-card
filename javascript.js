c238145bc501aed661d969a4e5cff661123320b0
bmovie92 <84676481+bmovie92@users.noreply.github.com>
2023-11-13 04:40:47 -0800

    first commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const button = document.querySelector("button")
+
+button.addEventListener("click",()=>{
+    alert("  SUCCESFULLY SUBMITTED")
+})
+
+const button1 = document.getElementById("btn1")
+button1.addEventListener("click" , () =>{
+    button1.style.backgroundColor ="orange";
+
+})
+const button2 = document.getElementById("btn2")
+button2.addEventListener("click" , () =>{
+    button2.style.backgroundColor ="orange";
+    button2.style.color ="hsl(210, 6%, 14%)"
+    
+})
+const button3 = document.getElementById("btn3")
+button3.addEventListener("click" , () =>{
+    button3.style.backgroundColor ="orange";
+
+})
+const button4 = document.getElementById("btn4")
+button4.addEventListener("click" , () =>{
+    button4.style.backgroundColor ="orange";
+
+})
+const button5 = document.getElementById("btn5")
+button5.addEventListener("click" , () =>{
+    button5.style.backgroundColor ="orange";
+
+})
